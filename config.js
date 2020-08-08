9a8b11f6681758bb04fec569119b5d8d8676e192
Mark Pham <markpham@Marks-iMac.local>
2020-08-08 16:27:15 -0700

    setting up for vercel deployment

---
+++ config.js
@@ -0,0 +1,4 @@
+// This is client side config only - don't put anything in here that shouldn't be public!
+// connects us to the graphQL yoga server so we can make queries and mutations!
+export const endpoint = `http://localhost:4444`;
+export const perPage = 4;
